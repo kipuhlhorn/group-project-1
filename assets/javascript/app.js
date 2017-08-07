dd794fccedc9e8f61cedc2af58299d7083109f45
JJ <jianan.beijing@gmail.com>
2017-08-06 23:50:32 -0700

    some changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,14 @@
-// $("button").on("click", function(){
-// This example displays an address form, using the autocomplete feature
-      // of the Google Places API to help users fill in the information.
+	var number;
+	var state_short;
+	var city;
+	var street;
+	var place;
+	var zipCode;
+	var latitude;
+	var longitude;
 
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. 
 	var placeSearch, autocomplete;
 	var componentForm = {
 	street_number: 'short_name',
@@ -27,46 +31,49 @@
 	// fields in the form.
 		autocomplete.addListener('place_changed', fillInAddress);
 	}
-
-	function fillInAddress() {
+		function fillInAddress() {
 	// Get the place details from the autocomplete object.
-		var place = autocomplete.getPlace();
+			place = autocomplete.getPlace();
 	        console.log(place);
 
+	}
 	        
 	// variables for zillow API to generate calling
-		var number= place.address_components[0].long_name;
-		var street = place.address_components[1].long_name;
-		var city = place.address_components[2].long_name;
-		var state_full = place.address_components[4].long_name;
-		var state_short = place.address_components[4].short_name;
-		var zipCode = place.address_components[6].long_name;
+		 number= place.address_components[0].long_name;
+		 street = place.address_components[1].long_name;
+		 city = place.address_components[2].long_name;
+		 state_short = place.address_components[4].short_name;
+		 zipCode = place.address_components[6].long_name;
 
 		// variables for crimespot API to generate calling;
-		var latitude = place.geometry.location.lat();
-		var longitude = place.geometry.location.lng();
+		latitude = place.geometry.location.lat();
+		longitude = place.geometry.location.lng();
 
-	}
 // replacing space to "+" 
-// 		 street = street.replace(" ","+");
-// 		 city = city.replace(" ","+");
+		 street = street.replace(" ","+");
+		 console.log(street);
 
-// // zillow API calling 
-// 		 var queryURL = "http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz1930iltfqiz_35s1w&address=" 
-// 	+ number + street + "&citystatezip=" + city + state_short;
+		 city = city.replace(" ","+");
 
-// 	$.ajax({
-// 		url:queryURL,
-// 		method: "GET"
-// 	}).done(function(data){
-// 		console.log(queryURL);
-// 		console.log(data);
-// 	})
+// trigger click event to call zillow API;
+$("button").on("click", function(event){
+	event.preventDefault();
+// zillow API calling 
+		 var queryURL = "http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz1930iltfqiz_35s1w&address=" 
+	+ number + street + "&citystatezip=" + city + state_short;
+
+	$.ajax({
+		url:queryURL,
+		method: "GET"
+	}).done(function(data){
+		console.log(queryURL);
+		console.log(data);
+	})；
 
 
 
 
-// });
+});
 
 
 
